cbf98daf6acce9c6c8dfcf44f8ff4af1caa4b015
hooxoo <ecstaiji@gmail.com>
2022-11-20 20:02:38 +0900

    zsh settings

---
+++ .config/slimzsh/slim.zsh
@@ -0,0 +1,45 @@
+slim_path=`dirname $0`
+fpath=( $slim_path $fpath )
+
+# export PATH=~/.local/bin:$PATH
+
+HISTFILE=$slim_path/.HISTFILE
+HISTSIZE=1000
+SAVEHIST=1000
+unsetopt beep
+bindkey -v
+
+autoload -U promptinit && promptinit
+prompt pure
+autoload -U colors && colors
+
+# setopt promptsubst
+
+# export STARSHIP_CONFIG=~/.config/starship/bubble.toml
+
+export FZF_DEFAULT_OPTS='--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4'
+
+autoload -Uz compinit
+compinit -C
+
+setopt autocd
+setopt extendedglob
+setopt NO_NOMATCH
+
+export CLICOLOR=1
+export LESSHISTFILE=-
+
+source $slim_path/keys.zsh
+source $slim_path/history.zsh
+source $slim_path/completion.zsh
+source $slim_path/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $slim_path/aliases.zsh
+source $slim_path/correction.zsh
+source $slim_path/sudo.plugin.zsh
+source $slim_path/stack.zsh
+source $slim_path/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+# source $slim_path/powerlevel10k/powerlevel10k.zsh-theme
+
+
+# eval "$(starship init zsh)"
+
